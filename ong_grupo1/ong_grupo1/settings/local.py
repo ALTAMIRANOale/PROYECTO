539c30ac1cf473bc02bdc0783e1d4c26907c440c
grupo1 <aaltamirano693@gmail.com>
2022-08-01 01:28:55 -0300

    controlando apps

--- ong_grupo1/ong_grupo1/settings/local.py
+++ ong_grupo1/ong_grupo1/settings/local.py
@@ -1,1 +1,21 @@
 from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ongdb',
+        'USER': 'root',
+        'PASSWORD': '28159357',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
